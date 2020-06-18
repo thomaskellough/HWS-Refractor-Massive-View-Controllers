59756ef6193a10da36153e35c033f1044951d043
Thomas Kellough <thomas.kellough@brandtinfo.com>
2020-06-18 12:42:10 -0500

    created bundle extension and separated view controllers into it's own group

--- HackingWithSwift/HackingWithSwift/ViewControllers/ViewController.swift
+++ HackingWithSwift/HackingWithSwift/ViewControllers/ViewController.swift
@@ -9,29 +9,13 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    var projects = [Project]()
+    let projects: [Project] = Bundle.main.decode(from: "projects.json")
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Hacking with Swift"
         navigationController?.navigationBar.prefersLargeTitles = true
-
-        guard let url = Bundle.main.url(forResource: "projects", withExtension: "json") else {
-            fatalError("Failed to locate projects.json in app bundle.")
-        }
-
-        guard let data = try? Data(contentsOf: url) else {
-            fatalError("Failed to load projects.json in app bundle.")
-        }
-
-        let decoder = JSONDecoder()
-
-        guard let loadedProjects = try? decoder.decode([Project].self, from: data) else {
-            fatalError("Failed to decode projects.json from app bundle.")
-        }
-
-        projects = loadedProjects
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
